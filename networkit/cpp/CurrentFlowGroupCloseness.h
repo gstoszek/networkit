5d5f227fbe1125a21a65a42161f5e3fc6b8dd8e5
gstoszek <37437776+gstoszek@users.noreply.github.com>
2018-10-26 12:46:56 +0200

    Reupload after merge

---
+++ networkit/cpp/CurrentFlowGroupCloseness.h
@@ -0,0 +1,84 @@
+/*
+ * CurrentFlowGroupCloseness.h
+ *
+ *      Author: gstoszek
+ */
+
+#ifndef CURRENTFLOWGROUPCLOSENESS_H_
+#define CURRENTFLOWGROUPCLOSENESS_H_
+
+#include "Centrality.h"
+#include "../algebraic/CSRMatrix.h"
+#include "../numerics/LAMG/Lamg.h"
+#include "EffectiveResistanceDistance.h"
+#include "ERDLevel.h"
+#include <armadillo>
+
+namespace NetworKit {
+    /*
+     *
+     */
+    class CurrentFlowGroupCloseness: public NetworKit::Centrality {
+
+    public:
+        /**
+         *
+         * @param G An connected unweighted graph.
+         * @param k Size of the group of nodes
+         * @param CB If equal 0 runs simply algorithm without coursing, atherwise sets a Coarsening Bound
+         * @
+         */
+        CurrentFlowGroupCloseness(const Graph& G,const count k = 2,const count CB = 2);
+        /**
+         * Computes group of size k with maximum closeness and coresponding value on the graph passed in the constructor.
+         */
+        void run();
+        /**
+         * Returns group of size k with maximum closeness.
+         */
+        std::vector<node> groupMaxCurrentFlowCloseness();
+        /**
+         * Returns maximum current flow group closeness
+         */
+        double getCFGCC();
+        /**
+         *Computes value for given group
+         */
+
+
+
+    private:
+
+        count k=2;
+        count CB=2;
+        count n;
+
+        double CFGCC;
+        std::vector<node> S;
+
+        std::vector<node> vList;
+        std::vector<std::vector<node>> TopMatch;
+        std::vector<ERDLevel> LevelList;
+
+        EffectiveResistanceDistance ERD;
+
+        arma::Mat<double> L;
+        arma::Mat<double> Adj;
+
+        void cleanNetwork();
+        void greedy(count n_peripheral_merges);
+        void computeInitialERD(count upperDegreeBound);
+        std::vector<std::vector<node>> updateTopMatch(count minDegree);
+        count updateMinDegree(count minDegree);
+        std::vector<std::pair<node,node>> updateMatching(std::vector<std::pair<count,count>> indices);
+        std::vector<std::pair<count,count>> peripheralCoarsingIndices();
+        std::vector<std::pair<count,count>> coarsingIndices(count cDegree,bool Random);
+        void uncoarse(node s,node v,count ID);
+        count mergePeripheralNodes();
+        void coarseLaplacian(std::vector<std::pair<count,count>> indices);
+
+
+    };
+
+} /* namespace NetworKit */
+#endif /* CURRENTFLOWGROUPCLOSENESS_H_ */
---
+++ networkit/cpp/EffectiveResistanceDistance.h
@@ -0,0 +1,65 @@
+/*
+* EffectiveResistanceDistance.h
+        *
+        *      Author: gstoszek
+*/
+
+#ifndef EFFECTIVERESISTANCEDISTANCE_H_
+#define EFFECTIVERESISTANCEDISTANCE_H_
+
+#include "Centrality.h"
+#include "../algebraic/CSRMatrix.h"
+#include "../numerics/LAMG/Lamg.h"
+#include <armadillo>
+
+namespace NetworKit {
+
+    class
+    EffectiveResistanceDistance{
+
+    public:
+      /**
+       *Constructor
+       */
+      EffectiveResistanceDistance(const count n = 0);
+      /**
+       *computes EffectiveResistanceDistanceMatrice
+       *
+       *@param Moore Penrose Pseudo Inverse Laplacian, expect a Laplacian of smaller or equal size as EffectiveResistanceDistanceMatrice
+       *@param vecOfNodes, expect a vector of nodes with equal size as Laplacian
+       */
+      void computeFromPinvL(arma::Mat<double> PinvLaplacian,std::vector<node> vecOfNodes);
+      /**
+       *executes first join operation
+       *
+       *@param vecOfNodes, expect a vector of nodes with equal size as Laplacian
+       *@param v existing
+       *@param w new node
+       *@param edgeWeight, weight of edge reintroduced
+       */
+      void firstJoin(std::vector<node> vecOfNodes,node v, node w,double edgeWeight);
+      /**
+       *executes edge fire operation
+       *
+       *@param vecOfNodes, expect a vector of nodes with equal size as Laplacian
+       *@param s and v, nodes of added edges
+       *@param edgeWeight, weight of edge reintroduced
+       */
+      void edgeFire(std::vector<node> vecOfNodes,node v, node w,double edgeWeight);
+      /**
+       *executes non bridge delete operation
+       *
+       *@param vecOfNodes, expect a vector of nodes with equal size as Laplacian
+       *@param s and v, nodes of added edges
+       *@param edgeWeight, weight of edge reintroduced
+       */
+      void nonBridgeDelete(std::vector<node> vecOfNodes,node v, node w,double edgeWeight);
+      /**
+       *EffectiveResistanceDistanceMatrice
+       */
+      arma::Mat<double> M;
+    };
+
+
+} /* namespace NetworKit */
+#endif /* EFFECTIVERESISTANCEDISTANCE_H_*/
